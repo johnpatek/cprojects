e77b85505d6978e0b7a2cb582a0748d1ad53bc49
John R. Patek <johnpatek2@gmail.com>
2020-04-23 01:45:37 -0700

    Updated unittest.c

--- project01/test/unittest.c
+++ project01/test/unittest.c
@@ -1,21 +1,12 @@
-#include "file.h"
 #include <test.h>
 
 
-TEST_CASE(add_test)
+TEST_CASE(blank)
 {
-    EXPECT_EQ(add(1,2),3);
-    EXPECT_NEQ(add(1,2),4);
-}
 
-TEST_CASE(sub_test)
-{
-    EXPECT_EQ(sub(2,1),1);
-    EXPECT_NEQ(sub(1,2),1);
 }
 
 UNIT_TEST
 {
-    TEST(add_test);
-    TEST(sub_test);
+    TEST(blank);
 }
